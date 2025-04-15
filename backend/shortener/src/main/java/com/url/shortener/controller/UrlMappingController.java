afdb8b6f956530af110314739a105a4f1b47b31e
Atharv Jagzap <atharvjagzap@gmail.com>
2025-04-15 15:03:33 +0530

    URL Analytics

--- backend/shortener/src/main/java/com/url/shortener/controller/UrlMappingController.java
+++ backend/shortener/src/main/java/com/url/shortener/controller/UrlMappingController.java
@@ -61,16 +61,16 @@ public class UrlMappingController {
     }
 //
 //
-//    @GetMapping("/totalClicks")
-//    @PreAuthorize("hasRole('USER')")
-//    public ResponseEntity<Map<LocalDate, Long>> getTotalClicksByDate(Principal principal,
-//                                                                     @RequestParam("startDate") String startDate,
-//                                                                     @RequestParam("endDate") String endDate){
-//        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
-//        User user = userService.findByUsername(principal.getName());
-//        LocalDate start = LocalDate.parse(startDate, formatter);
-//        LocalDate end = LocalDate.parse(endDate, formatter);
-//        Map<LocalDate, Long> totalClicks = urlMappingService.getTotalClicksByUserAndDate(user, start, end);
-//        return ResponseEntity.ok(totalClicks);
-//    }
+    @GetMapping("/totalClicks")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Map<LocalDate, Long>> getTotalClicksByDate(Principal principal,
+                                                                     @RequestParam("startDate") String startDate,
+                                                                     @RequestParam("endDate") String endDate){
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+        User user = userService.findByUsername(principal.getName());
+        LocalDate start = LocalDate.parse(startDate, formatter);
+        LocalDate end = LocalDate.parse(endDate, formatter);
+        Map<LocalDate, Long> totalClicks = urlMappingService.getTotalClicksByUserAndDate(user, start, end);
+        return ResponseEntity.ok(totalClicks);
+    }
 }
